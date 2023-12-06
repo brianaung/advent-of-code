e2438b12e5b5e7c55920f31740ae5f78aea14a60
Brian Aung <brianaung16@gmail.com>
2023-12-06 17:01:32 +1100

    clean up

--- 2023/day06/main.go
+++ 2023/day06/main.go
@@ -20,13 +20,7 @@ func partone(lines []string) {
 	for i := 0; i < len(times); i++ {
 		t, _ := strconv.Atoi(string(times[i]))
 		d, _ := strconv.Atoi(string(dis[i]))
-		ways, j := 0, 0
-		for j < t {
-			if (j * (t - j)) > d {
-				ways++
-			}
-			j++
-		}
+		ways := solve(t, d)
 		p1 *= ways
 	}
 	fmt.Println(p1)
@@ -35,12 +29,16 @@ func partone(lines []string) {
 func parttwo(lines []string) {
 	t, _ := strconv.Atoi(strings.Replace(strings.Split(lines[0], ":")[1], " ", "", -1))
 	d, _ := strconv.Atoi(strings.Replace(strings.Split(lines[1], ":")[1], " ", "", -1))
-	p2, i := 0, 0
+	fmt.Println(solve(t, d))
+}
+
+func solve(t int, d int) int {
+	i, ret := 0, 0
 	for i < t {
 		if (i * (t - i)) > d {
-			p2++
+			ret++
 		}
 		i++
 	}
-	fmt.Println(p2)
+	return ret
 }
